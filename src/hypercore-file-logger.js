3c475f790d661931b9f283bfc8b770bb15dd6444
vigan <vigan.abd@gmail.com>
2020-08-19 23:26:31 +0200

    file logger implemented

---
+++ src/hypercore-file-logger.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const debug = require('debug')('hcore-logger')
+const HyperCoreLogger = require('./hypercore-logger')
+const Tail = require('nodejs-tail')
+
+class HyperCoreFileLogger extends HyperCoreLogger {
+  /**
+   * @param {string} filepath Path that will be tailed
+   * @param {string|Function} feedDir
+   * @param {string|Buffer} [feedKey]
+   *
+   * @param {Object} [feedOpts]
+   * @param {boolean} [feedOpts.createIfMissing]
+   * @param {'json'|'utf-8'|'binary'} [feedOpts.valueEncoding]
+   * @param {boolean} [feedOpts.eagerUpdate]
+   * @param {string|Buffer} [feedOpts.secretKey]
+   * @param {boolean} [feedOpts.storeSecretKey]
+   * @param {number} [feedOpts.storageCacheSize]
+   * @param {(index: number, data: any, peer: Peer, cb: Function) => void} [feedOpts.onwrite]
+   * @param {boolean} [feedOpts.stats]
+   * @param {Object} [feedOpts.crypto]: {
+   * @param {(data: any, secretKey: string|Buffer, cb: Function) => void} feedOpts.crypto.sign
+   * @param {(signature: any, data: any, key: string|Buffer, cb: function name(params)} feedOpts.crypto.verify
+   * @param {Object} [feedOpts.noiseKeyPair]
+   * @param {string|Buffer} feedOpts.noiseKeyPair.publicKey
+   * @param {string|Buffer} feedOpts.noiseKeyPair.secretKey
+   *
+   * @param {Object} [swarmOpts]
+   * @param {string[]} [swarmOpts.bootstrap]
+   * @param {any} [swarmOpts.ephemeral]
+   * @param {number} [swarmOpts.maxPeers]
+   * @param {number} [swarmOpts.maxServerSockets]
+   * @param {number} [swarmOpts.maxClientSockets]
+   * @param {(peer: Peer) => boolean} [swarmOpts.validatePeer]
+   * @param {Object} [swarmOpts.queue]
+   * @param {number} swarmOpts.queue.requeue
+   * @param {Object} swarmOpts.queue.forget
+   * @param {number} swarmOpts.queue.forget.unresponsive
+   * @param {number} swarmOpts.queue.forget.banned
+   * @param {boolean} [swarmOpts.multiplex]
+   */
+  constructor (filepath, feedDir, feedKey = null, feedOpts = null, swarmOpts = null) {
+    super(feedDir, feedKey, feedOpts, swarmOpts)
+
+    this.tail = new Tail(filepath)
+    this.tail.on('line', (line) => {
+      const data = line + '\n'
+      this.feed.append(data)
+    })
+  }
+
+  async start () {
+    await super.start()
+    this.tail.watch()
+
+    debug('feed started listening for changes on %s', this.tail.filename)
+  }
+
+  async stop () {
+    this.tail.close()
+    await super.stop()
+  }
+}
+
+module.exports = HyperCoreFileLogger
