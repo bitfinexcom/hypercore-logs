bb4caef6fa496b5a1ea72e0477ebdcaf661dd3ed
vigan <vigan.abd@gmail.com>
2020-09-02 22:31:34 +0200

    path helpers extended, fullpath renamed to fullPath

--- src/helper.js
+++ src/helper.js
@@ -1,12 +1,77 @@
 'use strict'
 
+const fs = require('fs')
+const glob = require('glob')
 const path = require('path')
 
 /**
  * @param {string} pathlike
  * @returns {string}
  */
-const fullpath = (pathlike) => path.normalize(path.resolve(pathlike))
+const fullPath = (pathlike) => path.normalize(path.resolve(pathlike))
+
+/**
+ * @param {string} pathlike
+ * @returns {Promise<boolean>}
+ */
+const isDir = async (pathlike) => {
+  try {
+    const stats = await fs.promises.stat(pathlike)
+    return stats.isDirectory()
+  } catch (err) {
+    return false
+  }
+}
+
+/**
+ * @param {string} pathlike
+ * @returns {boolean}
+ */
+const isGlob = (pathlike) => /\*|\?|\^|!|\+|@|\[|\]/.test(pathlike)
+
+/**
+ * @param {string} pathlike
+ * @returns {Promise<boolean>}
+ */
+const fExists = async (pathlike) => {
+  try {
+    await fs.promises.stat(pathlike)
+    return true
+  } catch (err) {
+    if (err.code === 'ENOENT') return false
+    throw err
+  }
+}
+
+/**
+ * @param {string} pathlike
+ * @returns {Promise<string[]>}
+ */
+const globPath = async (pathlike) => {
+  const matches = await new Promise((resolve, reject) => {
+    glob(pathlike, (err, res) => err ? reject(err) : resolve(res))
+  })
+
+  return matches.map(p => fullPath(p))
+}
+
+/**
+ * @param {string} pathlike
+ * @returns {Promise<string[]>}
+ */
+const resolvePaths = async (pathlike) => {
+  if (isGlob(pathlike)) return globPath(pathlike)
+
+  if (!await fExists(pathlike)) return []
+
+  if (await isDir(pathlike)) {
+    const matches = await globPath(path.join(pathlike, '**/*'))
+    const dirs = await Promise.all(matches.map(p => isDir(p)))
+    return matches.filter((p, i) => dirs[i] === false)
+  }
+
+  return [fullPath(pathlike)]
+}
 
 /**
  * @param {string} str
@@ -16,6 +81,11 @@ const isHexStr = (str) => (typeof str === 'string' || str instanceof String) &&
   /^[0-9A-Fa-f]+$/.test(str)
 
 module.exports = {
-  fullpath,
+  fullPath,
+  isDir,
+  isGlob,
+  fExists,
+  globPath,
+  resolvePaths,
   isHexStr
 }
