3f735b0c5f952961e11f56096c84c5ec99f989d2
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2021-12-22 12:47:21 +0200

    save temporary files for benchmarks to logs directory

--- benchmarks/suits/file-logger.js
+++ benchmarks/suits/file-logger.js
@@ -10,7 +10,7 @@ const secretKey = 'f66a2bbec992b9d4c3dfadc67b8aa69782334ae7753fce182' +
   '016e06c80b6d0dd0db350f414a93274e5613930b' +
   '79c7226f21270cfc1435b96350422a0abf63dc2'
 
-const tailfile = path.join(cwd, 'file.log')
+const tailfile = path.join(cwd, 'logs', 'file.log')
 const server = new HyperCoreFileLogger(tailfile, true, feedDir, key, { secretKey })
 
 const startPromise = Promise.all([
