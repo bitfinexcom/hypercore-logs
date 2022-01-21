3350156f2b81a2bfb7c3696b83b4cad212f5850d
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2022-01-21 14:16:38 +0200

    add tests for startDate/endDate

--- test/logger.test.js
+++ test/logger.test.js
@@ -73,5 +73,40 @@ module.exports = () => {
       expect(databuff[0]).to.be.equal('some data 3')
       expect(databuff[1]).to.be.equal('some data 4')
     }).timeout(1200000)
+
+    it('filter logs by date', async () => {
+      const databuff = []
+
+      const server = new HyperCoreLogger(() => ram())
+      const push = (date) => server.feed.append(date + ' some data ')
+
+      await server.start()
+      push('1970-01-01T00:00:00.000Z')
+      push('1970-01-01T00:10:00.000Z')
+      push('1970-01-01T00:20:00.000Z')
+      push('1970-01-01T00:30:00.000Z')
+      push('1970-01-01T00:40:00.000Z')
+      push('1970-01-01T00:50:00.000Z')
+
+      const client = new HyperCoreLogReader(
+        () => ram(), server.feedKey, null, null, { startDate: new Date('1970-01-01T00:05:00.000Z'), endDate: new Date('1970-01-01T00:35:00.000Z') }
+      )
+      client.on('data', (data) => { databuff.push(data.toString()) })
+
+      await sleep(1500)
+      await client.start()
+      await sleep(3000)
+
+      await Promise.all([
+        server.stop(),
+        client.stop()
+      ])
+
+      expect(databuff).to.eql([
+        '1970-01-01T00:10:00.000Z some data ',
+        '1970-01-01T00:20:00.000Z some data ',
+        '1970-01-01T00:30:00.000Z some data '
+      ])
+    }).timeout(1200000)
   })
 }
