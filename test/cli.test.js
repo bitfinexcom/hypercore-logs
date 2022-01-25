b6741a1599917c4470c422420baf5c7d7b85a6b5
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2022-01-25 11:29:54 +0200

    test cli

---
+++ test/cli.test.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const sinon = require('sinon')
+const chai = require('chai')
+  .use(require('dirty-chai'))
+  .use(require('sinon-chai'))
+const ram = require('random-access-memory')
+const { expect } = chai
+const { sleep } = require('./helper')
+const { HyperCoreLogger } = require('../')
+
+process.env.NODE_ENV = 'test'
+
+module.exports = () => {
+  describe('logger cli', () => {
+    beforeEach(function () {
+      sinon.spy(console, 'log')
+    })
+
+    afterEach(function () {
+      console.log.restore()
+    })
+
+    it('should print the logs', async () => {
+      const server = new HyperCoreLogger(() => ram())
+      const push = (i) => server.feed.append('some data ' + i)
+
+      await server.start()
+      push(0)
+      push(1)
+      push(2)
+
+      process.argv = ['_', '__', 'read', '--key', server.feed.key.toString('hex')]
+      const { main } = require('../bin/hyperlog')
+
+      const { client } = await main()
+      await sleep(2000)
+
+      expect(console.log).to.be.called.with('some data 0')
+      expect(console.log).to.be.called.with('some data 1')
+      expect(console.log).to.be.called.with('some data 2')
+
+      await Promise.all([
+        server.stop(),
+        client.stop()
+      ])
+    }).timeout(120000)
+  })
+}
