7bb044477cb6da9571612596f1ec4b949ac8ebbc
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2022-01-31 16:38:28 +0200

    add tests for bisect

---
+++ test/bisect.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai')
+  .use(require('dirty-chai'))
+const { expect } = chai
+const { bisect } = require('../src/bisect')
+
+module.exports = () => {
+  describe('bisect tests', () => {
+    it('must find the nearest index', async () => {
+      const data = [1, 2, 3, 4, 5, 6, 7]
+
+      const index = await bisect(item => item < 4 ? -1 : 1, i => data[i], data.length)
+
+      expect(index).to.eq(data.indexOf(3))
+    })
+  })
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const bisectTests = require('./bisect.test')
 const helperTests = require('./helper.test')
 const loggerTests = require('./logger.test')
 const fileLoggerTests = require('./logger.file.test')
 const udpLoggerTests = require('./logger.udp.test')
 
 describe('*** Unit testing! ***', () => {
+  bisectTests()
   helperTests()
   loggerTests()
   fileLoggerTests()
