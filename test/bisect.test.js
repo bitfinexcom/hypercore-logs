69a78066dbf55b780f08c9856009b949d612b11a
Vitaliy Stoliarov <vitaliy.stoliarov@code-care.pro>
2022-02-04 20:17:50 +0200

    add 'out of bounds' test for bisect

--- test/bisect.test.js
+++ test/bisect.test.js
@@ -12,5 +12,13 @@ module.exports = () => {
 
       expect(index).to.eq(data.indexOf(3))
     })
+
+    it('out of bounds', async () => {
+      const data = [1, 2, 3, 4, 5, 6, 7]
+
+      const index = await bisect(item => item > 8 ? 1 : -1, i => data[i], data.length)
+
+      expect(index).to.eq(data.length - 1)
+    })
   })
 }
